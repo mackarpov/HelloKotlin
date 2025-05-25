6cb68a390f613fd2d8165ac8ee09c415baad9fe0
mackarpov <mackarpov@gmail.com>
2025-05-25 18:47:13 +0300

    Lesson HelloKotlin_4 added

--- src/main/kotlin/HelloKotlin_4.kt
+++ src/main/kotlin/HelloKotlin_4.kt
@@ -1,5 +1,13 @@
 package ru.netology
 
 fun main() {
-    
+    val itemPrice = 100
+    val itemCount = 11
+    val discount = 100
+    val discountStart = 1000
+
+    val totalPrice: Int = itemPrice * itemCount
+    val result = if (totalPrice > discountStart) totalPrice - discount else totalPrice
+
+    println(result)
 }
