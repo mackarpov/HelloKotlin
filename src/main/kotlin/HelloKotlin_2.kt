51de4cf8d77680fc095e0d7f1a9a4ca567b8440f
mackarpov <mackarpov@gmail.com>
2025-05-25 18:32:39 +0300

    Lesson HelloKotlin_3 added

--- src/main/kotlin/HelloKotlin_2.kt
+++ src/main/kotlin/HelloKotlin_2.kt
@@ -3,5 +3,5 @@ package ru.netology
 fun main() {
     val itemPrice: Int = 100
     val discountStart: Int = 1000
-    
+
 }
---
+++ src/main/kotlin/HelloKotlin_3.kt
@@ -0,0 +1,9 @@
+package ru.netology
+
+fun main() {
+    val itemPrice = 100
+    val itemCount = 11
+    val discount = 100
+    val discountStart = 1000
+
+}
